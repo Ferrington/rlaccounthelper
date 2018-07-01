a8441bfdfb33a7469d0561d458d648d8a853b6aa
Ferrington <c.bandurski@yahoo.com>
2018-07-01 01:57:45 -0500

    fixed curl - validate_steam_id

--- rlaccounthelper.php
+++ rlaccounthelper.php
@@ -1,32 +1,28 @@
 <?php
 
 $api_key = include('api_key.php');
-//$steam_id = $_POST['steam_id'];
+$steam_id = $_POST['steam_id'];
 
-$steam_id = "imatwig";
 //logic
-/*
 if ($_POST['method'] == 'validate') {
     echo validate_steam_id($steam_id);
 }
-*/
+
 //functions
 function validate_steam_id($id, $api_key) {
-    $fp = fopen(dirname(__FILE__).'/errorlog.txt', 'w');
     $curl = curl_init();
-    
+    $url = "https://api.rocketleaguestats.com/v1/player?unique_id=". $id ."&platform_id=1&apikey=". $api_key;
+
     curl_setopt_array($curl, array(
         CURLOPT_RETURNTRANSFER => 1,
-        CURLOPT_URL => "https://api.rocketleaguestats.com/v1/player?unique_id=". $id ."&platform_id=1",
+        CURLOPT_URL => $url
     ));
-    curl_setopt($curl, CURLOPT_USERPWD, $api_key);
-    curl_setopt($curl, CURLOPT_VERBOSE, 1);
-    curl_setopt($curl, CURLOPT_STDERR, $fp);
-    
-    $response = curl_exec($curl);
-    
-    return $response;
-    
-}
 
-echo validate_steam_id($steam_id, $api_key);
+    $response = json_decode(curl_exec($curl),true);
+
+    if (isset($response['displayName'])) {
+        return $response['displayName'];
+    } else {
+        return 'false';
+    }
+}
